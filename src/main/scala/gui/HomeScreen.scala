5405f2c6b8cb8c7df923903602602541edc10612
sygoh0909 <shuyigoh2004@gmail.com>
2025-07-09 22:28:31 +0800

    Updated structure

--- src/main/scala/gui/HomeScreen.scala
+++ src/main/scala/gui/HomeScreen.scala
@@ -1,5 +1,14 @@
 package gui
 
-class HomeScreen {
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
 
-}
+object HomeScreen extends JFXApp3:
+
+  override def start(): Unit =
+    stage = new PrimaryStage()
+
+
+end HomeScreen
+
+
