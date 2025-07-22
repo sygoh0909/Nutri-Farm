762963823b0435323164851eee62cc88ade57c2d
sygoh0909 <shuyigoh2004@gmail.com>
2025-07-23 00:31:02 +0800

    Updated main program to include css file

--- src/main/scala/program/MainProgram.scala
+++ src/main/scala/program/MainProgram.scala
@@ -10,6 +10,8 @@ object MainProgram extends JFXApp3:
   override def start(): Unit =
     stage = new PrimaryStage:
       title = "Nutri-Farm"
-      scene = new Scene(800, 600)
+      scene = new Scene(800, 600) {
+        stylesheets.add(getClass.getResource("/css/styles.css").toExternalForm)
+      }
 
     stage.scene().setRoot(Landing.build(stage)) // Change current screen to the landing page screen
