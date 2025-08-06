8b3df098b9d06f1e7be9eb99eb94909a98fdf5e1
sygoh0909 <shuyigoh2004@gmail.com>
2025-08-06 18:53:04 +0800

    Updated garden ui

--- src/main/scala/gui/Garden.scala
+++ src/main/scala/gui/Garden.scala
@@ -3,25 +3,19 @@ package gui
 import controller.GardenController
 import model.Player
 import scalafx.geometry.{Insets, Pos}
-import scalafx.scene.control.{Button, Label}
+import scalafx.scene.control.Label
 import scalafx.scene.layout.VBox
 import scalafx.stage.Stage
 
 object Garden:
 
   def build(stage: Stage, player: Player): VBox =
-    val gardenGrid = GardenController.buildGrid(stage, player) // Planning to implement in controller
-    val controlPanel = GardenController.buildControlPanel(stage, player) // Planning to implement in controller
-
-    // Back to home page
-    val backButton = new Button("Back to Home") {
-      styleClass.add("game-button")
-      onAction = _ => stage.scene().setRoot(Home.build(player, stage))
-    }
+    val gardenGrid = GardenController.buildGrid(stage, player)
+    val controlPanel = GardenController.buildControlPanel(stage, player)
 
     new VBox {
-      spacing = 30
-      padding = Insets(40)
+      spacing = 10
+      padding = Insets(30, 40, 30, 40)
       alignment = Pos.Center
       styleClass ++= Seq("bg-base", "garden-bg")
       children = Seq(
@@ -30,6 +24,5 @@ object Garden:
         },
         gardenGrid,
         controlPanel,
-        backButton
       )
     }
