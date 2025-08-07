37d6704f6ac52470ae7f8804f84315cf5fbf2e24
sygoh0909 <shuyigoh2004@gmail.com>
2025-08-07 15:30:13 +0800

    Added crop and crop registry to maintain crop better

---
+++ src/main/scala/model/Crop.scala
@@ -0,0 +1,12 @@
+package model
+
+enum CropType:
+  case Vegetable, Grain, Fruit // Can be expanded in future 
+
+trait Crop:
+  def name: String
+  def emoji: String
+  def nutrition: String
+  def calories: Double
+  def points: Int
+  def recipes: Seq[String]
---
+++ src/main/scala/model/CropRegistry.scala
@@ -0,0 +1,65 @@
+package model
+
+case object Carrot extends Crop {
+  val name: String = "Carrot"
+  val emoji: String = "🥕"
+  val nutrition: String = "Vitamin A"
+  val calories: Double = 25.0
+  val points: Int = 5
+  val cropType: CropType = CropType.Vegetable
+  val recipes: Seq[String] = Seq("Carrot Soup", "Carrot Cake")
+}
+
+case object Tomato extends Crop {
+  val name: String = "Tomato"
+  val emoji: String = "🍅"
+  val nutrition: String = "Vitamin C"
+  val calories: Double = 22.0
+  val points: Int = 6
+  val cropType: CropType = CropType.Vegetable
+  val recipes: Seq[String] = Seq("Tomato Pasta", "Tomato Soup")
+}
+
+case object Corn extends Crop {
+  val name: String = "Corn"
+  val emoji: String = "🌽"
+  val nutrition: String = "Carbohydrate"
+  val calories: Double = 35.0
+  val points: Int = 4
+  val cropType: CropType = CropType.Vegetable
+  val recipes: Seq[String] = Seq("Corn Tacos", "Corn Chowder")
+}
+
+case object Eggplant extends Crop {
+  val name: String = "Eggplant"
+  val emoji: String = "🍆"
+  val nutrition: String = "Antioxidants"
+  val calories: Double = 28.0
+  val points: Int = 6
+  val cropType: CropType = CropType.Vegetable
+  val recipes: Seq[String] = Seq("Eggplant Parmesan", "Eggplant Curry")
+}
+
+case object Cucumber extends Crop {
+  val name: String = "Cucumber"
+  val emoji: String = "🥒"
+  val nutrition: String = "Hydration"
+  val calories: Double = 18.0
+  val points: Int = 3
+  val cropType: CropType = CropType.Vegetable
+  val recipes: Seq[String] = Seq("Cucumber Salad", "Cucumber Sandwiches")
+}
+
+case object Wheat extends Crop {
+  val name: String = "Wheat"
+  val emoji: String = "🌾"
+  val nutrition: String = "Fiber"
+  val calories: Double = 40.0
+  val points: Int = 6
+  val cropType: CropType = CropType.Grain
+  val recipes: Seq[String] = Seq("Whole Wheat Bread", "Wheat Pancakes")
+}
+
+object CropRegistry:
+  val crops: Seq[Crop] = Seq(Carrot, Tomato, Corn, Eggplant, Cucumber, Wheat)
+  def getByName(name: String): Option[Crop] = crops.find(_.name == name)
