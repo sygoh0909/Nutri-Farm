7c8805ac5fe67791b0e5e660bc6c947c469f993e
sygoh0909 <shuyigoh2004@gmail.com>
2025-08-07 19:25:20 +0800

    Updated to include crop types

--- src/main/scala/db/FoodTable.scala
+++ src/main/scala/db/FoodTable.scala
@@ -1,18 +1,27 @@
 package db
 
-import slick.jdbc.PostgresProfile.api._ // Core Slick components
-import slick.lifted.{ProvenShape, Tag} // To use table/tag
-import model.FoodItem
+import slick.jdbc.PostgresProfile.api.*
+import slick.lifted.{ProvenShape, Tag}
+import model.{CropType, FoodItem}
+import slick.ast.BaseTypedType
+import slick.jdbc.JdbcType
+
+implicit val cropTypeMapper: JdbcType[CropType] with BaseTypedType[CropType] =
+  MappedColumnType.base[CropType, String](
+    e => e.toString, // Save as String
+    s => CropType.valueOf(s) // Convert String back to enum
+  )
 
 class FoodTable(tag: Tag) extends Table[FoodItem](tag, "food_items"): // Using slick FRM mapping
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def name = column[String]("name")
   def nutrition = column[String]("nutrition")
   def calories = column[Double]("calories")
+  def cropType = column[CropType]("crop_type")
   def playerID = column[Int]("player_id")
 
   def * : ProvenShape[FoodItem] =
-    (id, name, nutrition, calories, playerID) <> (FoodItem.apply.tupled, FoodItem.unapply)// Maps tuple (db row) into food item (object)
+    (id, name, nutrition, calories, cropType, playerID) <> (FoodItem.apply.tupled, FoodItem.unapply)// Maps tuple (db row) into food item (object)
 
 object FoodTable:
   val foodItem = TableQuery[FoodTable]
